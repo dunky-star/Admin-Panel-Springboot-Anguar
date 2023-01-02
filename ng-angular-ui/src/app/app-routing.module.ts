6e7b5b1b309d21d7edeca66c6e19e71b72011059
dunky-star <dukygeoffrey39@gmail.com>
2023-01-02 16:14:38 +0300

    Update route guard for users and admin pages

--- ng-angular-ui/src/app/app-routing.module.ts
+++ ng-angular-ui/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 import { ProductListComponent } from './components/admin/product-list/product-list.component';
 import { UserListComponent } from './components/admin/user-list/user-list.component';
@@ -11,6 +11,7 @@ import { LoginComponent } from './components/user/login/login.component';
 import { ProfileComponent } from './components/user/profile/profile.component';
 import { RegisterComponent } from './components/user/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
+import {Role} from './model/role';
 
 const routes: Routes = [
   //Main page
@@ -19,30 +20,37 @@ const routes: Routes = [
   //User pages
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'profile',
-  component: ProfileComponent,
-  canActivate: [AuthGuard]
-  },
   {path: 'detail', component: DetailComponent},
   {path: 'detail/:id', component: DetailComponent},
+    //error pages
+  {path: '404', component: NotFoundComponent},
+  {path: '401', component: UnauthorizedComponent},
+
+  // Below are pages that require login
+  {path: 'profile',
+  component: ProfileComponent,
+  data: {roles: [Role.ADMIN, Role.USER]}
+  },
 
   //admin panel
   {path: 'dashboard',
-  component: DashboardComponent
+  component: DashboardComponent,
+  canActivate: [AuthGuard],
+  data: {roles: [Role.ADMIN]}
 
   },
   {path: 'user-list',
-  component: UserListComponent
+  component: UserListComponent,
+  canActivate: [AuthGuard],
+  data: {roles: [Role.ADMIN]}
 
   },
   {path: 'product-list',
-  component: ProductListComponent
+  component: ProductListComponent,
+  canActivate: [AuthGuard],
+  data: {roles: [Role.ADMIN]}
 
- },
-
-  //error pages
-  {path: '404', component: NotFoundComponent},
-  {path: '401', component: UnauthorizedComponent}
+ }
 
 ];
 
@@ -50,4 +58,10 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+    constructor(private router: Router) {
+    this.router.errorHandler = (error: any) => {
+      this.router.navigate(['/404']);
+    }
+  }
+}
