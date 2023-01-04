475ee4e6b10be44d1b46b24694107894da42baea
dunky-star <dukygeoffrey39@gmail.com>
2023-01-04 16:34:26 +0300

    Product detail page components and view implementation

--- ng-angular-ui/src/app/components/user/detail/detail.component.ts
+++ ng-angular-ui/src/app/components/user/detail/detail.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  productId?: string;
+  currentProduct: Product;
+
+  constructor(private route: ActivatedRoute) {
+    this.currentProduct = JSON.parse(localStorage.getItem("currentProduct") as string);
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      if(params.has('id')){
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this.productId = params.get('id')!;
+      }
+    });
+  }
 
 }
