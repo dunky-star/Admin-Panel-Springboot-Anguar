96f60bb8e6f9d07296b632c4a66ea115d2bb6961
dunky-star <dukygeoffrey39@gmail.com>
2023-01-06 18:16:44 +0300

    Admin dashboard implementation

--- ng-angular-ui/src/app/components/admin/product-list/product-list.component.ts
+++ ng-angular-ui/src/app/components/admin/product-list/product-list.component.ts
@@ -1,10 +1,117 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Product } from 'src/app/model/product';
+import { AdminService } from 'src/app/services/admin.service';
+declare var $: any;
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  productList: Array<Product> = [];
+  dataSource: MatTableDataSource<Product> = new MatTableDataSource();
+  displayedColumns: string[] = ['id', 'name', 'price', 'action'];
+  selectedProduct: Product = new Product();
+  errorMessage?: string;
+  infoMessage?: string;
 
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort;
+
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit() {
+    this.findAllProducts();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  findAllProducts() {
+    this.adminService.findAllProducts().subscribe((data) => {
+      this.productList = data;
+      this.dataSource.data = data;
+    });
+  }
+
+  createNewProductRequest() {
+    this.selectedProduct = new Product();
+    $('#productModal').modal('show');
+  }
+
+  editProductRequest(product: Product) {
+    this.selectedProduct = product;
+    $('#productModal').modal('show');
+  }
+
+  saveProduct() {
+    if (!this.selectedProduct.id) {
+      this.createProduct();
+    } else {
+      this.updateProduct();
+    }
+  }
+
+  createProduct() {
+    this.adminService.createProduct(this.selectedProduct).subscribe({
+      next: (data) => {
+        this.productList.push(data);
+        this.dataSource = new MatTableDataSource(this.productList);
+        this.infoMessage = 'Mission is completed';
+        $('#productModal').modal('hide');
+      },
+       error: () => {
+        this.errorMessage = 'Unexpected error occurred.';
+      }
+   });
+  }
+
+  updateProduct() {
+    this.adminService.updateProduct(this.selectedProduct).subscribe({
+      next: () => {
+        let itemIndex = this.productList.findIndex(
+          (item) => item.id == this.selectedProduct.id
+        );
+        this.productList[itemIndex] = this.selectedProduct;
+        this.dataSource = new MatTableDataSource(this.productList);
+        this.infoMessage = 'Operation is completed';
+        $('#productModal').modal('hide');
+      },
+      error: () => {
+        this.errorMessage = 'Unexpected error occurred.';
+      }
+   });
+  }
+
+  deleteProductRequest(product: Product) {
+    this.selectedProduct = product;
+    $('#deleteModal').modal('show');
+  }
+
+  deleteProduct() {
+    this.adminService.deleteProduct(this.selectedProduct).subscribe({
+      next: () => {
+        let itemIndex = this.productList.findIndex(
+          (item) => item.id == this.selectedProduct.id
+        );
+        if (itemIndex !== -1) {
+          this.productList.splice(itemIndex, 1);
+        }
+        this.dataSource = new MatTableDataSource(this.productList);
+        this.infoMessage = 'Mission is completed';
+        $('#deleteModal').modal('hide');
+      },
+      error: () => {
+        this.errorMessage = 'Unexpected error occurred.';
+      }
+   });
+  }
 }
